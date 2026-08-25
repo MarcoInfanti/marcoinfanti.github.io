45b4807b73e6d35bb3a9817dda51b519ee0a33d7
Marco Infanti <Marcoinfanti1994@gmail.com>
2026-08-25 23:27:42 +0200

    Auto-generated commit

---
+++ chunk-E72NMGHW.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var o=[{path:"table",loadComponent:()=>import("./chunk-GXGPEK3T.js").then(e=>e.TableDemoComponent)},{path:"charts",loadComponent:()=>import("./chunk-ZMBUTJID.js").then(e=>e.ChartsDemoComponent)}],t=o;export{o as DEMOS_ROUTES,t as default};
